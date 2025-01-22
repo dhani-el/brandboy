28479fe8b2d34766ed3f9c01a857799b8843915f
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-01-22 04:09:29 +0100

    implement menu animation

--- src/Home/components.jsx
+++ src/Home/components.jsx
@@ -1,4 +1,5 @@
-
+import { useState } from "react"
+import {motion,stagger, animate} from "motion/react"
 
 
 export function Logo(){
@@ -24,3 +25,31 @@ function ANavLink({title,link}){
            </a>
 }
 
+export function Hamburger(){
+
+    const [darkenBackground,setDarkenBackground] = useState(false);
+
+    function handleHoverStart(){
+        setDarkenBackground(()=>true);
+        animate(".hamburger-line",{width:"100%",transition:{duration:0.5}},{delay:stagger(0.1)})
+    }
+    function handleHoverEnd(){
+        setDarkenBackground(()=>false);
+        animate(".hamburger-line",{width:"0%",transition:{duration:0.5}},{delay:stagger(0.1)})
+    }
+
+    return <motion.div  onHoverStart={()=>{handleHoverStart()}} onHoverEnd={()=>{handleHoverEnd()}} className="bg-white flex flex-col h-[3rem] justify-around p-4 relative">
+                <motion.div initial={{width:"0%"}} animate={(darkenBackground?{width:"100%",opacity:1,transition:{duration:0.5}}:{width:"0%",opacity:0,transition:{duration:0.5}})} className="absolute z-0 h-full bg-black right-0"></motion.div>
+                <OneHamburgerLine animate={darkenBackground}/>
+                <OneHamburgerLine animate={darkenBackground}/>
+                <OneHamburgerLine animate={darkenBackground}/>
+           </motion.div>
+}
+
+function OneHamburgerLine(){
+    return <motion.div className=" bg-black w-[1rem] h-[0.15rem] relative z-[1] ">
+                <motion.div initial={{width:"0%"}}  className="hamburger-line bg-white h-full ">
+
+                </motion.div>
+            </motion.div>
+}
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,13 @@
 
-import { Logo } from "./components";
+import { Logo,Hamburger } from "./components";
 
 export default function Home(){
-    return <div className="p-8 w-screen">
-              <div className="w-full flex items-center justify-center">
-                <Logo/>
+    return <div className="p-8 w-screen bg-[#f7f7f7]">
+              <div className="w-full flex items-center justify-between">
+                <div className="flex justify-center w-[90%]">
+                    <Logo/>
+                </div>
+                <Hamburger/>
               </div>
     </div>
 }
