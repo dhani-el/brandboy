5e7f53306d121b0dd8fb0eb03325eef653185e48
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-01-22 05:02:50 +0100

    implement menu component transition

--- src/Home/components.jsx
+++ src/Home/components.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import {motion,stagger, animate} from "motion/react"
 
 
 export function Logo(){
     return <a href="/" className="flex items-center">
-              <img src="/brandboy.jpg" className="w-[2rem] h-[2rem]" />
-              <p className="font-medium font-lexend text-2xl">BRANDBOY</p>
+              <img src="/brandboy.jpg" className="w-[2rem] h-[2rem] drop-shadow-lg" />
+              <p className="font-medium font-lexend text-2xl drop-shadow-lg">BRANDBOY</p>
             </a>
 }
 
@@ -28,28 +28,62 @@ function ANavLink({title,link}){
 export function Hamburger(){
 
     const [darkenBackground,setDarkenBackground] = useState(false);
+    const [openMenu,setOpenMenu] = useState(false);
 
     function handleHoverStart(){
         setDarkenBackground(()=>true);
         animate(".hamburger-line",{width:"100%",transition:{duration:0.5}},{delay:stagger(0.1)})
     }
+
     function handleHoverEnd(){
         setDarkenBackground(()=>false);
         animate(".hamburger-line",{width:"0%",transition:{duration:0.5}},{delay:stagger(0.1)})
     }
 
-    return <motion.div  onHoverStart={()=>{handleHoverStart()}} onHoverEnd={()=>{handleHoverEnd()}} className="bg-white flex flex-col h-[3rem] justify-around p-4 relative">
+    function handleClick(){
+        setOpenMenu(init=>!init)
+    }
+
+    function transitionToMenu(){
+        animate("#line2",{opacity:0, transition:{duration:0.2}})
+        animate("#line1",{y:"0.2rem",scaleX:1.2,scaleY:1.5, transition:{duration:0.5}})
+        animate("#line3",{y:"-0.45rem",scaleX:1.2,scaleY:1.5, transition:{duration:0.5}})
+        animate("#line1",{rotate:"140deg", transition:{duration:0.5}})
+        animate("#line3",{rotate:"-140deg", transition:{duration:0.5}})
+        animate("#menu",{display:"block",opacity:1,height:"100%", transition:{duration:0.5}})
+    }
+
+    function transitionFromMenu(){
+        animate("#line1",{y:"0rem",scaleX:1,scaleY:1, transition:{duration:0.5}})
+        animate("#line3",{y:"0rem",scaleX:1,scaleY:1, transition:{duration:0.5}})
+        animate("#line1",{rotate:"0deg", transition:{duration:0.5}})
+        animate("#line3",{rotate:"0deg", transition:{duration:0.5}})
+        animate("#line2",{opacity:1, transition:{duration:0.2}})
+        animate("#menu",{display:"none",opacity:0,height:"0%", transition:{duration:0.5}})
+    }
+
+    useEffect(()=>{
+        openMenu?transitionToMenu():transitionFromMenu()
+    },[openMenu])
+
+    return <motion.div onClick={handleClick}  onHoverStart={()=>{handleHoverStart()}} onHoverEnd={()=>{handleHoverEnd()}} className="bg-white flex flex-col h-[3rem] justify-around p-4 relative z-10">
                 <motion.div initial={{width:"0%"}} animate={(darkenBackground?{width:"100%",opacity:1,transition:{duration:0.5}}:{width:"0%",opacity:0,transition:{duration:0.5}})} className="absolute z-0 h-full bg-black right-0"></motion.div>
-                <OneHamburgerLine animate={darkenBackground}/>
-                <OneHamburgerLine animate={darkenBackground}/>
-                <OneHamburgerLine animate={darkenBackground}/>
+                <OneHamburgerLine id={"line1"}  animate={darkenBackground}/>
+                <OneHamburgerLine id={"line2"} animate={darkenBackground}/>
+                <OneHamburgerLine id={"line3"} animate={darkenBackground}/>
            </motion.div>
 }
 
-function OneHamburgerLine(){
-    return <motion.div className=" bg-black w-[1rem] h-[0.15rem] relative z-[1] ">
+function OneHamburgerLine({id}){
+    return <motion.div id={id} className=" bg-black w-[1rem] h-[0.15rem] relative z-[1] ">
                 <motion.div initial={{width:"0%"}}  className="hamburger-line bg-white h-full ">
 
                 </motion.div>
             </motion.div>
+}
+
+export function MenuComponent(){
+    return <motion.div id="menu" initial={{display:"none",opacity:0,height:"0%"}} className=" w-screen absolute left-0 bottom-0 bg-slate-300 z-[5]">
+
+    </motion.div>
 }
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,13 +1,14 @@
 
-import { Logo,Hamburger } from "./components";
+import { Logo,Hamburger,MenuComponent } from "./components";
 
 export default function Home(){
-    return <div className="p-8 w-screen bg-[#f7f7f7]">
+    return <div className="p-8 w-screen bg-white relative h-screen">
               <div className="w-full flex items-center justify-between">
                 <div className="flex justify-center w-[90%]">
                     <Logo/>
                 </div>
                 <Hamburger/>
+                <MenuComponent/>
               </div>
     </div>
 }
