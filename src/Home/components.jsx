0b9c7ea08bc62605f400e0412e7094a5c793afad
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-01-22 01:56:49 +0100

    import text for brand boy

---
+++ src/Home/components.jsx
@@ -0,0 +1,8 @@
+
+
+
+export function Logo(){
+    return <a href="/">
+              <p className="font-medium font-lexend text-lg">BRANDBOY</p>
+            </a>
+}
---
+++ src/Home/index.jsx
@@ -0,0 +1,9 @@
+
+import { Logo } from "./components"
+
+
+export default function Home(){
+    return <div className="p-8">
+              <Logo/>
+    </div>
+}
