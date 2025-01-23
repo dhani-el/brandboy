81c7ebc8c2ad0cdb4a6521c7624b2df5666aee8b
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-01-23 21:33:25 +0100

    implement all animations for the home page that do not rely on scrolling

--- src/Home/components.jsx
+++ src/Home/components.jsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react"
 import {motion,stagger, animate} from "motion/react"
 
 import MenuImage from "../assets/Images/menuImage.jpg"
+import landingVideo from "../assets/videos/video-cuisiniste-lyon-italian-kitchen.mp4"
+
+
+export function Header(){
+    return <div id="header" className=" relative z-[1] bg-slate-100 w-full flex items-center justify-center px-6 py-4">
+                <Logo/>
+
+  </div>
+}
 
 export function Logo(){
     return <a href="/" className="flex items-center">
@@ -46,12 +55,12 @@ export function Hamburger(){
     }
 
     function transitionToMenu(){
-        animate("#line2",{opacity:0, transition:{duration:0.2}})
-        animate("#line1",{y:"0.2rem",scaleX:1.2,scaleY:1.5, transition:{duration:0.5}})
-        animate("#line3",{y:"-0.45rem",scaleX:1.2,scaleY:1.5, transition:{duration:0.5}})
-        animate("#line1",{rotate:"140deg", transition:{duration:0.5}})
-        animate("#line3",{rotate:"-140deg", transition:{duration:0.5}})
-        animate("#menu",{display:"flex",opacity:1,height:"100%", transition:{duration:0.5}})
+        animate("#line2",{opacity:0, transition:{duration:0.2}});
+        animate("#line1",{y:"0.2rem",scaleX:1.2,scaleY:1.5, transition:{duration:0.5}});
+        animate("#line3",{y:"-0.45rem",scaleX:1.2,scaleY:1.5, transition:{duration:0.5}});
+        animate("#line1",{rotate:"140deg", transition:{duration:0.5}});
+        animate("#line3",{rotate:"-140deg", transition:{duration:0.5}});
+        animate("#menu",{display:"flex",opacity:1,height:"100vh", transition:{duration:0.5,ease:""}});
     }
 
     function transitionFromMenu(){
@@ -67,7 +76,7 @@ export function Hamburger(){
         openMenu?transitionToMenu():transitionFromMenu()
     },[openMenu])
 
-    return <motion.div onClick={handleClick}  onHoverStart={()=>{handleHoverStart()}} onHoverEnd={()=>{handleHoverEnd()}} className="bg-white flex flex-col h-[3rem] justify-around p-4 relative z-10">
+    return <motion.div onClick={handleClick}  onHoverStart={()=>{handleHoverStart()}} onHoverEnd={()=>{handleHoverEnd()}} className="fixed right-[2rem] top-[0.5rem] bg-white flex flex-col h-[3rem] justify-around p-4  z-10">
                 <motion.div initial={{width:"0%"}} animate={(darkenBackground?{width:"100%",opacity:1,transition:{duration:0.5}}:{width:"0%",opacity:0,transition:{duration:0.5}})} className="absolute z-0 h-full bg-black right-0"></motion.div>
                 <OneHamburgerLine id={"line1"}  animate={darkenBackground}/>
                 <OneHamburgerLine id={"line2"} animate={darkenBackground}/>
@@ -84,7 +93,7 @@ function OneHamburgerLine({id}){
 }
 
 export function MenuComponent(){
-    return <motion.div id="menu" initial={{display:"none",opacity:0,height:"0%"}} className="cursor-pointer w-screen absolute left-0 bottom-0 bg-white z-[5] p-16 justify-between items-center">
+    return <motion.div id="menu" initial={{display:"none",opacity:0,height:"0%"}} className="cursor-pointer w-screen fixed left-0 bottom-0 bg-white z-[2] p-16 justify-between items-center">
                 <motion.div className="w-[25%] h-full overflow-hidden flex items-center justify-center ">
                     <motion.img src={MenuImage} className="h-full max-w-none"/>
                 </motion.div>
@@ -98,17 +107,17 @@ export function MenuComponent(){
                     </motion.div>
                     <motion.div>
                         <div className="flex">
-                            <motion.div>
+                            <motion.div className="border-l-[0.06rem] border-gray-300 border-solid">
                                 <AMenuLinkItemSmall text={"About Us"} id={"About"}/>
                                 <AMenuLinkItemSmall text={"Our Brands"} id={"Brands"}/>
                                 <AMenuLinkItemSmall text={"Showroom"} id={"Showroom"}/>
                             </motion.div>
-                            <motion.div>
+                            <motion.div className="border-l-[0.06rem] border-gray-300 border-solid">
                                 <AMenuLinkItemSmall text={"Your Project"} id={"roject"}/>
                                 <AMenuLinkItemSmall text={"Personalization"} id={"Personalization"}/>
                                 <AMenuLinkItemSmall text={"Layout"} id={"Layout"}/>
                             </motion.div>
-                            <motion.div>
+                            <motion.div className="border-l-[0.06rem] border-gray-300 border-solid">
                                 <AMenuLinkItemSmall text={"Instagram"} id={"Instagram"}/>
                                 <AMenuLinkItemSmall text={"Facebook"} id={"Facebook"}/>
                                 <AMenuLinkItemSmall text={"Houzz"} id={"Houzz"}/>
@@ -166,4 +175,10 @@ function AMenuLinkItemSmall({link,text,id}){
                 <p className="text-sm font-lexend font-light">{text}</p>
                 <motion.hr id={id} className="h-[0.1rem] w-[0%] bg-red-500" />
            </motion.a>
+}
+
+export function Attraction() {
+    return <div className="absolute z-0 w-screen top-0 left-0 overflow-hidden">
+                <video autoPlay muted loop  src={landingVideo} ></video>
+            </div>
 }
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,14 +1,11 @@
 
-import { Logo,Hamburger,MenuComponent } from "./components";
+import { Header,Attraction,MenuComponent,Hamburger } from "./components";
 
 export default function Home(){
-    return <div className="p-8 w-screen bg-white relative h-screen">
-              <div className="w-full flex items-center justify-between">
-                <div className="flex justify-center w-[90%]">
-                    <Logo/>
-                </div>
-                <Hamburger/>
-                <MenuComponent/>
-              </div>
+    return <div className="w-screen bg-white relative h-screen">
+              <Header/>
+              <MenuComponent/>
+              <Attraction/>
+              <Hamburger/>
     </div>
 }
